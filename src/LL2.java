9f26824cb887be1a7d23d00856c563fc1188d8e9
Ratik Tiwari <ratik.tiwari@newgensoft.com>
2022-05-20 19:12:29 +0530

    new commit

---
+++ src/LL2.java
@@ -0,0 +1,44 @@
+public class LL2 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode middleNode(ListNode head) {
+
+        ListNode temp = head;
+        if(head.next==null){ //only one node
+            return head;
+        }
+        ListNode i =head;
+        if(head.next.next==null){
+            return i.next;
+        }
+        ListNode j = head.next.next;
+
+        while(i!=null && j!=null){
+            i=i.next;
+            if(j.next!=null){
+                j=j.next.next;
+            }
+            else{
+                return i;
+            }
+        }
+        return i.next;
+    }
+
+}
