895cbe4e465d51026e237f56f77ab26132d72c94
Ratik Tiwari <ratik.tiwari@newgensoft.com>
2022-05-20 19:35:25 +0530

    new commit

---
+++ src/LL3.java
@@ -0,0 +1,24 @@
+public class LL3 {
+
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+
+    public void deleteNode(ListNode node) {
+
+        // stupid approach
+//            ListNode nextNode = node.next;
+//
+//            while(nextNode.next!=null){
+//                node.val = nextNode.val;
+//                node=node.next;
+//                nextNode = nextNode.next;
+//            }
+//
+//            node.val = nextNode.val;
+//            node.next=null;
+
+    }
+}
