1a8cc77257e8729444d6ef2c530c45b60a10fee7
Ratik Tiwari <ratik.tiwari@newgensoft.com>
2022-05-20 19:13:15 +0530

    new commit

--- src/LL2.java
+++ src/LL2.java
@@ -19,26 +19,39 @@ public class LL2 {
 
     public ListNode middleNode(ListNode head) {
 
-        ListNode temp = head;
-        if(head.next==null){ //only one node
-            return head;
-        }
-        ListNode i =head;
-        if(head.next.next==null){
-            return i.next;
-        }
-        ListNode j = head.next.next;
+//        ListNode temp = head;
+//        if(head.next==null){ //only one node
+//            return head;
+//        }
+//        ListNode i =head;
+//        if(head.next.next==null){
+//            return i.next;
+//        }
+//        ListNode j = head.next.next;
+//
+//        while(i!=null && j!=null){
+//            i=i.next;
+//            if(j.next!=null){
+//                j=j.next.next;
+//            }
+//            else{
+//                return i;
+//            }
+//        }
+//        return i.next;
 
-        while(i!=null && j!=null){
-            i=i.next;
-            if(j.next!=null){
-                j=j.next.next;
-            }
-            else{
-                return i;
-            }
+
+
+
+
+        //same strategy as above but less code lines
+
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        return i.next;
+        return slow;
     }
 
 }
