758e7c04b58b2ff6dd7b0356e17bc4b893c50d25
Ratik Tiwari <ratiktiwari@gmail.com>
2022-05-21 17:35:26 +0530

    Time: 8 ms (75.41%), Space: 281.9 MB (55.37%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -11,30 +11,27 @@
 class Solution {
     public ListNode mergeNodes(ListNode head) {
         
-        if (head.next == null) {
-            return null;
-        }
-
-        ListNode iterator = head;
-        ListNode newList = new ListNode(0);
-        ListNode newTemp = newList;
-
-        int sum = 0;
-
-        while (iterator != null) {
-            sum += iterator.val;
-
-            if (iterator.val == 0) {
-                if (sum != 0) {
-                    newTemp.next = new ListNode(sum);
-                    newTemp=newTemp.next;
-                }
-                sum = 0;
+        ListNode first = head;
+        ListNode second = head.next;
+        int sum=0;
+        
+        
+        while(second!=null){
+            
+            sum+=second.val;
+            
+            if(second.val==0){
+                first.next.val=sum;
+                first=first.next;
+                sum=0;
             }
-            iterator = iterator.next;
-
+            
+            second=second.next;
+            
         }
+        
+        first.next = null;
 
-        return newList.next;
+        return head.next;
     }
 }
