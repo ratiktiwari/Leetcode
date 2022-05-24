9f73ab8f9ea4da1632bda6e7314a31154067d02c
Ratik Tiwari <ratiktiwari@gmail.com>
2022-05-24 18:58:11 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (78.76%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,30 +1,20 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         
-        if(m==0){
-            for(int i=0; i<n; i++){
-                nums1[i] = nums2[i];
+        int nums1Iterator = m - 1;
+        int nums2Iterator = n - 1;
+        int emptyIterator = m + n - 1;
+
+        while (nums2Iterator >= 0 && nums1Iterator >= 0) {
+            if (nums1[nums1Iterator] <= nums2[nums2Iterator]) {
+                nums1[emptyIterator--] = nums2[nums2Iterator--];
+            } else {
+                nums1[emptyIterator--] = nums1[nums1Iterator--];
             }
-            return;
         }
 
-        int nums1Iterator = m-1;
-        int nums2Iterator = n-1;
-        int emptyIterator = m+n-1;
-
-        while(nums2Iterator>=0){
-            if(nums1[nums1Iterator]<=nums2[nums2Iterator]){
-                nums1[emptyIterator--] = nums2[nums2Iterator--];
-            }
-            else{
-                nums1[emptyIterator--] = nums1[nums1Iterator--];
-                if(nums1Iterator<0){
-                    for(int i=nums2Iterator; i>=0; i--){
-                        nums1[emptyIterator--] = nums2[i];
-                    }
-                    return;
-                }
-            }
+        while (nums2Iterator >= 0) {
+            nums1[emptyIterator--] = nums2[nums2Iterator--];
         }
     }
 }
