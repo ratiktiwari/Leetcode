55d9ad0541c7415dbc93e05b6baa86771d587f20
ratik.tiwari <ratiktiwari@gmail.com>
2022-05-27 20:38:12 +0530

    new commit

---
+++ src/PascalsTriangle.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PascalsTriangle {
+
+//    public List<List<Integer>> generate(int numRows) {
+//
+//        List<List<Integer>> array = new ArrayList<>();
+//
+//
+//        if (numRows == 1) {
+//            array.add(Arrays.asList(1));
+//            return array;
+//        }
+//
+//        array.add(Arrays.asList(1));
+//        array.add(Arrays.asList(1,1));
+//
+//        for(int )
+//
+//
+//    }
+
+}
---
+++ src/ValidSudoku.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+
+public class ValidSudoku {
+
+    public boolean isValidSudoku(char[][] board) {
+
+        HashSet set = new HashSet<>();
+        char num = 0;
+
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+
+                num = board[i][j];
+                if((num !='.')){
+
+                    if(!set.add(num + " in row " + i) || !set.add(num + " in column " + j) || !set.add(num + " in block[" + i/3 + "," + j/3 + "]")){
+
+                        return false;
+
+                    }
+
+                }
+
+            }
+        }
+
+        return true;
+
+    }
+
+}
