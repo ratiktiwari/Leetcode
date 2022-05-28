f8b4bc41ab52872aea4177b9a2377edba8fd62bd
ratik.tiwari <ratiktiwari@gmail.com>
2022-05-28 21:18:01 +0530

    new commit

---
+++ src/RansomNote.java
@@ -0,0 +1,20 @@
+public class RansomNote {
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        int[] hashMap = new int[26];
+
+        for (int i = 0; i < magazine.length(); i++) {
+            hashMap[magazine.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if(--hashMap[ransomNote.charAt(i)-'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+}
