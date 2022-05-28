c0c99922fd88ac891e1d3c0b46e7715596e5f75f
ratik.tiwari <ratiktiwari@gmail.com>
2022-05-28 21:33:43 +0530

    new commit

---
+++ src/ValidAnagram.java
@@ -0,0 +1,23 @@
+public class ValidAnagram {
+
+    public boolean isAnagram(String s, String t) {
+
+        int[] hashMap = new int[26];
+
+        for(int i=0; i<s.length(); i++){
+            hashMap[s.charAt(i)-'a']++;
+        }
+        for(int i=0; i<t.length(); i++){
+            hashMap[t.charAt(i)-'a']--;
+        }
+        for(int i=0; i<26; i++){
+            if(hashMap[i]!=0){
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+}
