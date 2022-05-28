d5be93e769fbcaf267e62a00f2f975eb3052a9a6
Ratik Tiwari <ratiktiwari@gmail.com>
2022-05-28 21:14:18 +0530

    Time: 3 ms (87.88%), Space: 45.7 MB (61.11%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -2,20 +2,16 @@ class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         int[] hashMap = new int[26];
 
-        for(int i=0; i<ransomNote.length(); i++){
-            hashMap[ransomNote.charAt(i)-'a']++;
-        }
 
-        for(int i=0; i<magazine.length(); i++){
-            hashMap[magazine.charAt(i)-'a']--;
+        for (int i = 0; i < magazine.length(); i++) {
+            hashMap[magazine.charAt(i) - 'a']++;
         }
-
-        for(int i=0; i<26; i++){
-            if(hashMap[i]>0){
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if(--hashMap[ransomNote.charAt(i)-'a'] < 0) {
                 return false;
             }
         }
-        
+
         return true;
     }
 }
