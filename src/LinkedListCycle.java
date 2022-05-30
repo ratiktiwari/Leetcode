507c4686eba82ad624641c20c9aec7aa118a2fef
ratik.tiwari <ratiktiwari@gmail.com>
2022-05-30 20:10:04 +0530

    new commit

---
+++ src/LinkedListCycle.java
@@ -0,0 +1,25 @@
+public class LinkedListCycle {
+
+    public boolean hasCycle(ListNode head) {
+
+        if(head==null || head.next==null){
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(slow!=fast && fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+
+        if(slow==fast){
+            return true;
+        }
+
+        return false;
+
+    }
+
+}
